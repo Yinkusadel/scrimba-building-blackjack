72e704683b01ece427ec687eef7bdffb8ab9f786
yinkusadel <adeleyeolayinka73@gmail.com>
2023-08-05 13:21:39 +0100

    refactor: cards and sum variables so that the game can start

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+console.log(cards)
 
 function getRandomCard() {
-    // if 1     -> return 11
-    // if 11-13 -> return 10
     let randomNumer = Math.floor( Math.random()*13 ) + 1
     if (randomNumer > 10) {
         return 10
@@ -24,6 +21,13 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    // Generate two random numbes
+    // Re-assign the cards and sum variables so that the game can start
     renderGame()
 }
 
